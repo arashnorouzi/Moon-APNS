a16c216c2c9943589bb669a2081d737a15428485
Arash Norouzi <toofan164@hotmail.com>
2011-08-09 23:27:02 +1000

    First Commit by Arash

---
+++ Feedback.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MoonAPNS
+{
+	/// <summary>
+	/// Feedback object
+	/// </summary>
+	public class Feedback
+	{
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public Feedback()
+		{
+			this.DeviceToken = string.Empty;
+			this.Timestamp = DateTime.MinValue;
+		}
+
+		/// <summary>
+		/// Device Token string in hex form without any spaces or dashes
+		/// </summary>
+		public string DeviceToken
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Timestamp of the Feedback for when Apple received the notice to stop sending notifications to the device
+		/// </summary>
+		public DateTime Timestamp
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace MoonAPNS
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // var payload1 = new NotificationPayload("Device token","Message",Badge,"Sound");
+            var payload1 = new NotificationPayload("Device Token", "Message", 1, "default");
+            payload1.AddCustom("RegionID", "IDQ10150");
+
+            var p = new List<NotificationPayload> {payload1};
+
+            var push = new PushNotification(false, "p12 file location","password");
+            var rejected = push.SendToApple(p);
+            foreach (var item in rejected)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
