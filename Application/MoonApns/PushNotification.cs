5f532163897681d898ec7ca81ec9979dd6e94fa7
scart <dmitry.lipatov@iage.net>
2014-11-05 14:30:30 +0300

    Change protocol to TLS
    Apple has changed the server security and you have to change from SSL to
    TLS.
    
    http://stackoverflow.com/questions/18893347/a-call-to-sspi-failed-see-inner-exception

--- Application/MoonApns/PushNotification.cs
+++ Application/MoonApns/PushNotification.cs
@@ -229,7 +229,7 @@ namespace MoonAPNS
 
       try
       {
-        _apnsStream.AuthenticateAsClient(host, certificates, System.Security.Authentication.SslProtocols.Ssl3, false);
+        _apnsStream.AuthenticateAsClient(host, certificates, System.Security.Authentication.SslProtocols.Tls, false);
       }
       catch (System.Security.Authentication.AuthenticationException ex)
       {
